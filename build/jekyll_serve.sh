2429a509446ce5fcf9b26519a5664d00fb28348e
Peter Forret <peter@forret.com>
2023-09-27 20:03:39 +0200

    MOD: jekyll_serve.sh gitploy.sh

--- build/jekyll_serve.sh
+++ build/jekyll_serve.sh
@@ -2,4 +2,4 @@
 SCRIPT_DIR="$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")"
 "$SCRIPT_DIR/_open_browser.sh" "$JEKYLL_PORT"
 
-bundle exec jekyll serve --watch -- port "$JEKYLL_PORT"
+bundle exec jekyll serve --watch --port "$JEKYLL_PORT"
--- gitploy.sh
+++ gitploy.sh
@@ -25,7 +25,7 @@ option|l|log_dir|folder for log files |$HOME/log/$script_prefix
 option|t|tmp_dir|folder for temp files|/tmp/$script_prefix
 option|F|FRAMEWORK|laravel/mkdocs/jekyll/hugo (default: autodetect))|
 option|H|HUGO_PORT|hugo runs on port|1313
-option|J|JEKYLL_PORT|jekyll runs on port|8000
+option|J|JEKYLL_PORT|jekyll runs on port|4000
 option|L|LARAVEL_PORT|laravel runs on port|8000
 option|M|MKDOCS_PORT|mkdocs runs on port|4000
 choice|1|action|action to perform|push,pull,install,serve,check,env,update
